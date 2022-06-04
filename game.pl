a0965bb0e213dd5fff9643b9a0ac3fc7dd567d3f
Francesco <francesco.mistri98@gmail.com>
2022-06-04 18:10:01 +0200

    Added turn into the game position
    turn can be "us" or "them". The new game position is in the form nim(T, Pos).

--- game.pl
+++ game.pl
@@ -1,13 +1,14 @@
 % Nim game predicates (move generators, terminal positions, ...)
-% A nim position is represented by the term nim([n_0, ..., n_k]) where n_i
-% represents the number of matches left in row i.
+% A nim position is represented by the term nim(turn, [n_0, ..., n_k]) where n_i
+% represents the number of matches left in row i. turn is one of ["us", "them"].
 
 % Move generator
-moves(nim(Pos), nim(Next)) :-
+moves(nim(T, Pos), nim(T1, Next)) :-
 	gennext(Pos, Next),
 	ldiff(Pos, Next, Diff),
 	c0(Diff, Count),
-	Count == 1.
+	Count == 1,
+	nextturn(T, T1).
 
 % Difference between numeric lists
 % Both [H| L] and [H1| L1] must be fully instantiated
@@ -24,3 +25,7 @@ gennext([H| L], [H1| L1]) :- between(0, H, H1), gennext(L, L1).
 c0([], 0).
 c0([H| L], N) :- H == 0, !, c0(L, N).
 c0([H| L], N) :- H > 0, !, c0(L, N1), N is N1 + 1.
+
+
+nextturn(us, them).
+nextturn(them, us).
